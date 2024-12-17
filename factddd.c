ecd70a33a7e777aaf667803d498d73a4ffbafd5d
Md. An Nahian Prince <annahian44@gmail.com>
2024-12-17 11:34:35 +0600

    Add files via upload

---
+++ factddd.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    printf("%d\n",fact(n));
+    return 0;
+}
+
+int fact(n)
+{
+    if(n<=1)
+    {
+        return 1;
+    }
+    else
+    {
+        return n*fact(n-1);
+    }
+}
