97b8fcdabc45396f95ad52ea858af0efe2ee2faa
Md. An Nahian Prince <annahian44@gmail.com>
2025-01-19 01:38:52 +0600

    Create fib.c

---
+++ Discrete_Mathematics/fib.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    printf("%d ",fib(n));
+}
+
+int fib(int n)
+{
+    if(n==0)
+    {
+        return 0;
+    }
+    else if(n==1)
+    {
+        return 1;
+    }
+    else
+    {
+        return fib(n-1)+fib(n-2);
+    }
+}
