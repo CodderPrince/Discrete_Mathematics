6dd36aabecfc5862bbf6390d5a0a926a94a76564
Md. An Nahian Prince <annahian44@gmail.com>
2025-01-19 01:39:14 +0600

    Create prime33.c

---
+++ Discrete_Mathematics/prime33.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(prime(n))
+    {
+        printf("It is a prime number\n");
+    }
+    else
+    {
+        printf("It is not a prime number\n");
+    }
+    //printf("%d\n",prime(n));
+    return 0;
+}
+
+int prime(n)
+{
+    int i;
+    if(n<=1)
+    {
+        return 0;
+    }
+    for(i=2; i*i<=n; i++)
+    {
+        if(n%i==0)
+        {
+            return 0;
+        }
+    }
+    if(i*i>n)
+    {
+        return 1;
+    }
+}
