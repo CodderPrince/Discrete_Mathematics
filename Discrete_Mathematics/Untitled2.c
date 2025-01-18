a6d4e4da350e255ab54008a6d18cf97e387589db
Md. An Nahian Prince <annahian44@gmail.com>
2025-01-19 01:39:44 +0600

    Create Untitled2.c

---
+++ Discrete_Mathematics/Untitled2.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    //What will be the values stored in the variables year and count when the data 1988, x
+//is keyed in as a response to the following statements:
+//What is the output of the following program ?
+
+ unsigned x=1;
+ signed char y=-1;
+ if(x>y)
+ printf("x>y");
+ else
+ printf("x<=y");
+}
+//Did you expect this output ? Explain
+
+/*int x=10;
+float y=4.25;
+x = (int)y%x;
+printf("x == %d",x);
+
+int year = 1988;
+char code='x';
+scanf("%d %c",&year,&code);
+printf("%d %c",year,code);
+scanf("%c %d",&year, &code);
+printf("%c %d",year,code);
+scanf("%d %c",&code, &year);
+printf("%d %c",code,year);
+scanf("%s %c", &year, &code);
+printf("%s %c",year,code);
+}*/
