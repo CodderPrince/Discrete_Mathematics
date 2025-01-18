0428f47fa26d58cb107754320876595f84dc4083
Md. An Nahian Prince <annahian44@gmail.com>
2025-01-19 01:39:28 +0600

    Create sum222.c

---
+++ Discrete_Mathematics/sum222.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    printf("%d\n",sum(n));
+    return 0;
+}
+int sum(n)
+{
+    if(n==0)
+    {
+        return 0;
+    }
+    else
+    {
+        return n+sum(n-1);
+    }
+}
